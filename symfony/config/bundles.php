6b2785f40b413b111a3c1ba6a69e22e752d27291
Tich53 <rdouette@yahoo.fr>
2023-02-26 22:43:23 +0100

    EasyAdmin

--- symfony/config/bundles.php
+++ symfony/config/bundles.php
@@ -17,4 +17,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
