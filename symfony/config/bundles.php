f7acf790d1bf16026150661b9e38a539ce53dfc4
Tich53 <rdouette@yahoo.fr>
2023-02-26 17:51:11 +0100

    Login form authenticator && JWT token

--- symfony/config/bundles.php
+++ symfony/config/bundles.php
@@ -16,4 +16,5 @@ return [
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
