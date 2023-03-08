a3d8ba720bd2fa33c822ca6d4456bf991df02aa2
Richard Douetté <richard.douette@all4tec.net>
2023-03-08 14:05:13 +0100

    Add navbar && change theme to "journal" && add some CSS classes

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent],
+  declarations: [AppComponent, LoginComponent, NavbarComponent],
   imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
