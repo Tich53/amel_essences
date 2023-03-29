ce00907c64c026ac41fd92d6fb64aa5c3aa7ac19
Tich53 <rdouette@yahoo.fr>
2023-03-29 21:45:55 +0200

    Add normalization group ['product:read'] && disable api platform pagination

--- symfony/src/Entity/Gender.php
+++ symfony/src/Entity/Gender.php
@@ -11,6 +11,7 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\Common\Collections\ArrayCollection;
 use Gedmo\Timestampable\Traits\TimestampableEntity;
 use Gedmo\SoftDeleteable\Traits\SoftDeleteableEntity;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: GenderRepository::class)]
 #[ApiResource(
@@ -30,6 +31,7 @@ class Gender
     private ?int $id = null;
 
     #[ORM\Column(length: 45)]
+    #[Groups(['product:read'])]
     private ?string $name = null;
 
     #[ORM\OneToMany(mappedBy: 'gender', targetEntity: Product::class)]
