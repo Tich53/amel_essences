36b395bd460dcd79850c36e7585768aeb3f1b5bc
Richard Douetté <richard.douette@all4tec.net>
2023-05-10 18:43:48 +0200

    Add recap by user

--- angular/src/app/home/recap/recap.component.ts
+++ angular/src/app/home/recap/recap.component.ts
@@ -5,8 +5,9 @@ import {
   OnChanges,
   SimpleChanges,
 } from '@angular/core';
+import { User } from 'src/app/_interfaces/_abstracts/user/user';
 import { MainOrder } from 'src/app/_interfaces/main-order';
-import { Order } from 'src/app/_interfaces/order';
+import { OrderItem } from 'src/app/_interfaces/order-item';
 
 @Component({
   selector: 'app-recap',
@@ -16,19 +17,47 @@ import { Order } from 'src/app/_interfaces/order';
 export class RecapComponent implements OnInit, OnChanges {
   @Input() mainOrders?: MainOrder[];
 
-  orders: Order[] = [];
+  orderItemsByUser: OrderItem[] = [];
 
   constructor() {}
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['mainOrders']) {
-      console.log(this.mainOrders);
+      this.mainOrders?.sort((a, b) => b.id - a.id);
     }
   }
 
   ngOnInit(): void {}
 
-  showOrderItems(order: Order) {
-    order.show = !order.show;
+  isPending(mainOrder: MainOrder): boolean {
+    const now = new Date();
+    if (
+      Date.parse(now.toString()) < Date.parse(mainOrder.closingDate.toString())
+    ) {
+      return true;
+    }
+    return false;
+  }
+
+  getOrderItemsByUser(mainOrder: MainOrder): Map<User, OrderItem[]> {
+    const userByOrderItem = new Map<User, OrderItem[]>();
+    for (const order of mainOrder.orders) {
+      for (const orderItem of order.orderItems) {
+        const user = Array.from(userByOrderItem.keys()).find(
+          (u) => u.id === order.userAccount.id
+        );
+        let orderItems: OrderItem[] | undefined;
+        if (user) {
+          orderItems = userByOrderItem.get(user);
+        }
+        if (!orderItems) {
+          orderItems = [];
+          userByOrderItem.set(order.userAccount, orderItems);
+        }
+        orderItems.push(orderItem);
+      }
+    }
+    console.log(userByOrderItem);
+    return userByOrderItem;
   }
 }
